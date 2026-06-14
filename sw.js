1930d9acd6d8170249a4688e12c73d44c06dab6e
Adjimo <132588802+Adjimo@users.noreply.github.com>
2026-06-14 15:36:25 +0200

    V4.2

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // et se met à jour automatiquement dès qu'une nouvelle version est déployée.
 // ============================================================================
 
-const VERSION = 'orion-v19';
+const VERSION = 'orion-v22';
 const ASSETS = [
   './',
   './index.html',
